d97420fd3b59cea5585520a8867559a84766f663
Zerox <103904922+zer0xtech@users.noreply.github.com>
2026-03-23 13:07:12 +0100

    feat: started admin dashboard

---
+++ dashboard_admin.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+    <title>Accueil</title>
+    <link rel="stylesheet" href="style_web.css" />
+</head>
+
+<body>
+    <?php require_once 'includes/navbar.php'; ?>
+
+
+    <div class="buttons-container">
+        <button>Test</button>
+        <button>Test</button>
+        <button>Test</button>
+    </div>
+</body>
+
+</html>
